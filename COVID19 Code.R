134c03e62dd369570785c15fdaee3290e6e63752
Rutvij <44467789+RutvijBhutaiya@users.noreply.github.com>
2020-04-13 11:36:14 +0530

    Upadted NLP

--- COVID19 Code.R
+++ COVID19 Code.R
@@ -68,7 +68,7 @@ View(Health_India)
 
 ## 2. The Times of India; @timesofindia : India News
 
-TOI = searchTwitter('from:@timesofindia', 1000, lang = 'en', 
+TOI = searchTwitter('from:@timesofindia', 2000, lang = 'en', 
                     since = '2020-03-12', until = '2020-04-12')
 
 
@@ -77,10 +77,10 @@ TOI = do.call('rbind', lapply(TOI, as.data.frame))
 View(TOI)
 
 
-## 3. CNN Breaking News: @cnnbrk : The USA News
+## 3. CNN News: @CNN : The USA News
 
 
-CNN = searchTwitter('from:@cnnbrk', 1000, lang = 'en', 
+CNN = searchTwitter('from:@CNN', 2000, lang = 'en', 
                     since = '2020-03-12', until = '2020-04-12')
 
 
@@ -89,10 +89,10 @@ CNN = do.call('rbind', lapply(CNN, as.data.frame))
 View(CNN)
 
 
-## 4. BBC Breaking News: @BBCBreaking : UK News
+## 4. BBC News: @BBCNews : UK News
 
 
-BBC = searchTwitter('from:@BBCBreaking', 1000, 
+BBC = searchTwitter('from:@BBCNews', 1000, 
                     since = '2020-03-12', until = '2020-04-12')
 
 
@@ -111,12 +111,12 @@ View(BBC)
 
 # Ministry of Health India : 1.5 million
 # Times Of India: 12.6 million
-# CNN Breaking News: 57.3 million
-# BBC Breaking News: 42.6 million
+# CNN News: 47 million
+# BBC News: 27.6 million
 
 
 
-followeres = c(1500000, 12600000, 57300000, 42600000)
+followeres = c(1500000, 12600000, 47000000, 27600000)
 
 
 ## Retweet Counts
@@ -152,20 +152,231 @@ retweet_ratio = (retweet_counts/followeres)*100
 
 fav_ratio = (favourited_counts/followeres)*100
 
-Names = c('Ministry of Health India', 'Times Of India', 'CNN Breaking News', 'BBC Breaking News')
+Names = c('Ministry of Health India', 'Times Of India', 'CNN News', 'BBC News')
 
 
 
 Full_View = data.frame(Names, followeres, retweet_counts, retweet_ratio, favourited_counts, fav_ratio)
 
-###########################################################################################
+write.csv(Full_View, 'Tweet Account Stats.csv')
 
+###########################################################################################
+###########################################################################################
 
 ## Word Cloud
 
+library(quanteda)
+
+
+# For Ministry of Health India
+
+health_Dfm = dfm(as.character(Health_India$text), 
+            remove = c("amp", "rt", "https", "t.co", "will", "@MoHFW_INDIA", ":", '.', ',', ';', '-', '&','.',
+                       remove_numbers = TRUE, 
+                       remove_punct = TRUE,
+                       stem = TRUE,
+                       remove_symbols = TRUE, stopwords("english")))
+
+View(health_Dfm)
+
+topfeatures(health_Dfm, 20)
+
+textplot_wordcloud(health_Dfm, min.freq = 5, max_words = 500, random.order = FALSE, color = c('coral', 'seagreen'))
 
 
 
 
+## FOr Times Of India 
+
+
+toi_Dfm = dfm(as.character(TOI$text), 
+                 remove = c("amp", "rt", "https", "t.co", "will", "@timesofindia", ":", '.', ',', ';', '-', '&','.',
+                            remove_numbers = TRUE, 
+                            remove_punct = TRUE,
+                            stem = TRUE,
+                            remove_symbols = TRUE, stopwords("english")))
+
+View(toi_Dfm)
+
+topfeatures(toi_Dfm, 20)
+
+textplot_wordcloud(toi_Dfm, min.freq = 5, max_words = 500, random.order = FALSE, color = c('coral', 'seagreen'))
+
+
+
+## For CNN News
+
+
+cnn_Dfm = dfm(as.character(CNN$text), 
+              remove = c("amp", "rt", "https", "t.co", "will", "@CNN", ":", '.', ',', ';', '-', '&','.',
+                         remove_numbers = TRUE, 
+                         remove_punct = TRUE,
+                         stem = TRUE,
+                         remove_symbols = TRUE, stopwords("english")))
+
+View(cnn_Dfm)
+
+topfeatures(cnn_Dfm, 20)
+
+textplot_wordcloud(cnn_Dfm, min.freq = 5, max_words = 500, random.order = FALSE, color = c('coral', 'seagreen'))
+
+
+
+## FOr BBC News
+
+
+bbc_Dfm = dfm(as.character(BBC$text), 
+              remove = c("amp", "rt", "https", "t.co", "will", "@BBCNews", ":", '.', ',', ';', '-', '&','.', '"',
+                         remove_numbers = TRUE, 
+                         remove_punct = TRUE,
+                         stem = TRUE,
+                         remove_symbols = TRUE, stopwords("english")))
+
+View(bbc_Dfm)
+
+topfeatures(bbc_Dfm, 20)
+
+textplot_wordcloud(bbc_Dfm, min.freq = 5, max_words = 500, random.order = FALSE, color = c('coral', 'seagreen'))
+
+
+############################################################################################################################
+#############################################################################################################################
+
+
+
+## Sentiment Analysis
+
+
+## For Ministry of Health India
+
+
+library(syuzhet)
+
+health_text = as.character(Health_India$text)
+
+#3 Text Cleaning
+
+health_text = gsub("(RT|via)((?:\\b\\w*@\\w+)+)","",Health_India$text)
+health_text = gsub("http[^[:blank:]]+","",health_text)
+health_text = gsub("@\\w+","",health_text)
+health_text = gsub("[[:punct:]]"," ",health_text)
+health_text = gsub("[^[:alnum:]]"," ",health_text)
+
+health_text = as.character(health_text)
+
+
+# Get Sentiment Analysis
+
+health_senti = get_nrc_sentiment(health_text)
+
+# Bar Plot
+
+barplot(sort(colSums(prop.table(health_senti[, 1:10]))), 
+         cex.names = 0.7, 
+         las = 1, 
+         main = "Ministry of Health Tweet Sentiment Analysis", xlab="Percentage")
+
+
+
+# **************************************************************
+
+
+## For The Times Of india
+
+toi_text = as.character(TOI$text)
+
+#3 Text Cleaning
+
+toi_text = gsub("(RT|via)((?:\\b\\w*@\\w+)+)","",TOI$text)
+toi_text = gsub("http[^[:blank:]]+","",toi_text)
+toi_text = gsub("@\\w+","",toi_text)
+toi_text = gsub("[[:punct:]]"," ",toi_text)
+toi_text = gsub("[^[:alnum:]]"," ",toi_text)
+
+toi_text = as.character(toi_text)
+
+
+# Get Sentiment Analysis
+
+toi_senti = get_nrc_sentiment(toi_text)
+
+# Bar Plot
+
+barplot(sort(colSums(prop.table(toi_text[, 1:10]))), 
+        cex.names = 0.7, 
+        las = 1, 
+        main = "The Times of India Tweet Sentiment Analysis", xlab="Percentage")
+
+
+
+# *********************************************
+
+## For CNN News
+
+cnn_text = as.character(CNN$text)
+
+#3 Text Cleaning
+
+cnn_text = gsub("(RT|via)((?:\\b\\w*@\\w+)+)","",CNN$text)
+cnn_text = gsub("http[^[:blank:]]+","",cnn_text)
+cnn_text = gsub("@\\w+","",cnn_text)
+cnn_text = gsub("[[:punct:]]"," ",cnn_text)
+cnn_text = gsub("[^[:alnum:]]"," ",cnn_text)
+
+cnn_text = as.character(cnn_text)
+
+
+# Get Sentiment Analysis
+
+cnn_senti = get_nrc_sentiment(cnn_text)
+
+# Bar Plot
+
+barplot(sort(colSums(prop.table(cnn_text[, 1:10]))), 
+        cex.names = 0.7, 
+        las = 1, 
+        main = "CNN News Tweet Sentiment Analysis", xlab="Percentage")
+
+
+
+# **********************************
+
+## For BBC News
+
+
+bbc_text = as.character(BBC$text)
+
+#3 Text Cleaning
+
+bbc_text = gsub("(RT|via)((?:\\b\\w*@\\w+)+)","",BBC$text)
+bbc_text = gsub("http[^[:blank:]]+","",bbc_text)
+bbc_text = gsub("@\\w+","",bbc_text)
+bbc_text = gsub("[[:punct:]]"," ",bbc_text)
+bbc_text = gsub("[^[:alnum:]]"," ",bbc_text)
+
+bbc_text = as.character(bbc_text)
+
+
+# Get Sentiment Analysis
+
+bbc_senti = get_nrc_sentiment(bbc_text)
+
+# Bar Plot
+
+barplot(sort(colSums(prop.table(bbc_text[, 1:10]))), 
+        cex.names = 0.7, 
+        las = 1, 
+        main = "BBC News Tweet Sentiment Analysis", xlab="Percentage")
+
+
+
+#################################################################################################################
+#################################################################################################################
+
+
+
+## Key Topic Modeling Analysis 
+
+## For Ministry of Health India
 
 
